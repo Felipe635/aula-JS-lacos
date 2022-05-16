c7e059aaa3f8ece8ec1fed33531d70a1dc71af0b
Felipe635 <102757641+Felipe635@users.noreply.github.com>
2022-05-16 14:18:57 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,43 @@
+//Exercicio 1
+
+console.log('Exercicío 1');
+
+let c = 1;
+
+while(c <= 100){
+    console.log(c);
+    c = c + 1;
+}
+
+//Exercicio 2
+
+console.log('Exercicío 2');
+
+let c1 = 50;
+
+while(c1 <= 100){
+    console.log(c1);
+    c1 = c1 + 1;
+}
+
+//Exercicio 3
+
+console.log('Exercicío 3');
+
+let c2 = 10;
+
+while(c2 >= 0){
+    console.log(c2);
+    c2 = c2 - 1;
+}
+
+//Exercicio 4
+
+console.log('Exercicío 4');
+
+let c3 = 0;
+
+while(c3 <= 20){
+    console.log(c3);
+    c3 = c3 + 2;
+}
